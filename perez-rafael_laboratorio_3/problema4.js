a29823e95d94d44f80d201975b08e6bbfe62fc13
Raphael Perez <ralphperez.g@gmail.com>
2022-04-08 02:05:30 -0500

    problema 1 y 3

--- perez-rafael_laboratorio_3/problema4.js
+++ perez-rafael_laboratorio_3/problema4.js
@@ -1,3 +1,7 @@
 // Escribir una función que, dado un número "n" ( 0 < n < 1000000), retorne la sumatoria de todos los número primos debajo de ese número.
 // Ejemplo: Dado 7
 // Se debería retornar 18, ya que: 1 + 2 + 3 + 5 + 7 = 1
+const principal4 = (() => {
+
+
+})()
