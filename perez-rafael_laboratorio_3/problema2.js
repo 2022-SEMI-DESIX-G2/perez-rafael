5cf3f3669d36e0ff8032fb3e4b251826d9265f51
Raphael Perez <ralphperez.g@gmail.com>
2022-04-08 19:35:08 -0500

    terminar lab 2 y 4

--- perez-rafael_laboratorio_3/problema2.js
+++ perez-rafael_laboratorio_3/problema2.js
@@ -1,7 +1,21 @@
 // Escribir una función que, dada una cadena "t", retorne la cantidad de caracteres de la cadena.
 // Ejemplo: Dado AABBBACAA
 // Se debería retornar que hay 5 A, 3 B, 1 C. El tipo de dato del retorno es a discreción del estudiante
-const principal2 = (() => {
+class Problem2 {
 
+    constructor() {
 
-})()
+        console.log(this.CCaracteres());
+
+    }
+    CCaracteres() {
+        let texto = prompt('Escriba una serie de caracteres');
+        return [...texto.replace(/\s/g, '')].reduce((objeto, caracter) => {
+            objeto[caracter] = objeto[caracter] + 1 || 1;
+            console.log(`La cadena ${texto} se divede de la siguente manera`)
+            return objeto;
+        }, {});
+    }
+
+}
+var problem2 = new Problem2();
